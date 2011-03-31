3694b210a5334fcdc116cf5ba9a05cebc738a002
Tobias Koch <tobias@tobijk.de>
2011-03-31 23:19:35 +0300

    add primitive draft version of Scheduler

---
+++ lib/lemon/scheduler.rb
@@ -0,0 +1,86 @@
+#-*- encoding: utf-8 -*-
+#
+# This file is part of the Lemon Network Monitor.
+# Copyright (C) 2011, Tobias Koch <tobias@tobijk.de>
+#
+# Lemon is licensed under the GNU General Public License, version 2. A copy of
+# the license text can be found in the file LICENSE in the source distribution.
+#
+
+require 'lemon/periodic_task'
+require 'lemon/check'
+
+module Lemon
+
+  class Scheduler
+
+    def initialize(globals, tasks)
+      @config = globals
+      @tasks = tasks
+      @stop = false
+    end
+
+    def run
+      th = Thread.new do
+        work
+      end
+
+      return th
+    end
+
+    # This needs to be rewritten:
+    #
+    # * it's missing any sort of rate limiting,
+    #
+    # * it should be made event-driven using 'select' or 'poll'
+    #   on the local pipe ends of the tasks
+    #
+    def work
+      status_to_string = {
+        Lemon::Check::PASS => "SUCCESS",
+        Lemon::Check::WARN => "WARNING",
+        Lemon::Check::FAIL => "FAILED"
+      }
+
+      while !@stop
+        current_time = Time.now
+
+        active_tasks.each do |t|
+          t.update
+          if t.finished?
+            $stdout.write("#{status_to_string[t.exit_code]}: #{t.class} - #{t.message.split(/\n/)[0]}\n")
+          end
+        end
+
+        now = Time.now
+        waiting_tasks.each do |t|
+          if t.next_update < now
+            t.spawn
+          end
+        end
+
+        sleep 1
+      end
+
+      # terminate all active checks the hard way
+      active_tasks.each do |t|
+        t.kill "KILL"
+        t.wait
+      end
+    end
+
+    def stop!
+      @stop = true
+    end
+
+    def active_tasks
+      @tasks.select { |t| t.active?  }
+    end
+
+    def waiting_tasks
+      @tasks.select { |t| !t.active? }
+    end
+
+  end
+
+end
